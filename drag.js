811747c9263f9c57cbaa8e54a68f967ad018abe0
jack4toning <18200388927@163.com>
2017-11-15 16:34:17 +0800

    success drag

--- drag.js
+++ drag.js
@@ -5,13 +5,16 @@ function drag(id){
     var obj = document.getElementById(id);
     var disX = 0;
     var disY = 0;
-    obj.onmousedown = function () {
+    obj.onmousedown = function (ev) {
         disX = ev.pageX - obj.offsetLeft;
         disY = ev.pageY - obj.offsetTop;
         document.onmousemove = function (ev) {
             obj.style.left = ev.pageX - disX + 'px';
             obj.style.top = ev.pageY - disY + 'px';
-            alert(3000);
+        };
+        document.onmouseup = function () {
+          document.onmousemove = null;
+          document.onmouseup = null;
         }
     }
 }
